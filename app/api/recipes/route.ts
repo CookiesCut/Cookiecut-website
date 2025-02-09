fcef2e19a8d3b347c5f6b3d79644ff8d1d306056
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-02-09 18:57:01 +0330

    update api

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,18 +1,6 @@
-// pages/api/recipes/[id].ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import recipes from '../../../lib/recipes.json';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query;
-  
-  if (req.method === 'GET') {
-    const recipe = recipes.find((recipe) => recipe.id === id);
-    if (recipe) {
-      res.status(200).json(recipe);
-    } else {
-      res.status(404).json({ message: 'Recipe not found' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
+export async function GET() {
+  return NextResponse.json(recipes);
 }
